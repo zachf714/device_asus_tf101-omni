7eb889c23f407e30e32d054223730df62af27cf9
Jostein Kjønigsen <jostein@kjonigsen.net>
2013-05-12 16:21:39 +0200

    Revert "Add CM's GSM Support"
    This reverts commit 4e9b5572c11b6ded20ebe5f137439aeb9b5bf749.

--- cm.mk
+++ cm.mk
@@ -1,26 +1,8 @@
-## Copyright (C) 2012 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# Inherit device configuration for ALL tf101.
+# Inherit device configuration for tf101.
 $(call inherit-product, device/asus/tf101/full_tf101.mk)
 
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-
-# Inherit device configuration for tf101 gsm.
-$(call inherit-product, vendor/cm/config/gsm.mk)
 
 #
 # Setup device specific product configuration.
